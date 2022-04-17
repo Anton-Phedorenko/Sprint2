d0d3edb44c45814147fa97843b8bb12b0c13094a
Vladimir Semkin <vnsemkin@gmail.com>
2022-04-17 19:00:31 +0300

    "Version 4.1"

--- src/Main.java
+++ src/Main.java
@@ -38,11 +38,11 @@ public class Main {
     }
 
     private static void printInfo() {
-        System.out.println("1. Считать все месячные отчёты");
-        System.out.println("2. Считать годовой отчёт");
-        System.out.println("3. Сверить отчёты");
-        System.out.println("4. Вывести информацию о всех месячных отчётах");
-        System.out.println("5. Вывести информацию о годовом отчёте");
-        System.out.println("6. Выход из системы ");
+        System.out.println("1: Считать все месячные отчёты");
+        System.out.println("2: Считать годовой отчёт");
+        System.out.println("3: Сверить отчёты");
+        System.out.println("4: Вывести информацию о всех месячных отчётах");
+        System.out.println("5: Вывести информацию о годовом отчёте");
+        System.out.println("6: Выход из системы ");
     }
 }
